5ed3ccbdc1f5b2d7e3c668440a44c4a7faa7d34d
SophieeV <sophievesin@wanadoo.fr>
2021-06-14 12:01:19 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExerciceD1</title>
+</head>
+<body>
+    
+<?php 
+
+$months = array ('janvier', 'février', 'mars','avril','mai', 'juin','juillet','aout','septembre','octobre','novembre','décembre');
+
+echo $months[2];
+
+
+?>
+
+
+
+
+</body>
+</html>
+
+
